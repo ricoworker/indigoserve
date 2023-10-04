3af36d43b47ed1105b9ce9e54a0d30e806d1285d
ricokurnia12 <ricokurnia19@gmail.com>
2023-10-04 17:00:06 +0700

    add sedding2

--- seeders/20231004080533-seederpeformance.js
+++ seeders/20231004080533-seederpeformance.js
@@ -4,7 +4,18 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
    await queryInterface.bulkInsert('synergis',[
-
+{
+  telkom : "ini telkom",
+  entity:'ini entity',
+  model:'ini model',
+  lead_time:22,
+  output:'ini output',
+  confidence_level:'ini cofidence level',
+  project_status:'project status',
+  initial_date:'12-06-2022',
+  description:'ini deskripsi dari seeding',
+  id_startups:3
+}
    ])
   },
 
